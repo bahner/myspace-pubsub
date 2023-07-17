fbf7a2f0a1981fbe392c49d1e4ec94e59e1bdfb0
Lars Bahner <lars.bahner@gmail.com>
2023-07-17 04:13:34 +0200

    Create two config items for the API. URI doesn't understand ws: schema

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,8 @@ config :ex_ipfs,
   api_url: "http://127.0.0.1:5001/api/v0"
 
 config :ex_ipfs_pubsub,
-  api_url: "ws://127.0.0.1:5002/topic"
+  api_url: "http://127.0.0.1:5002/api/v0",
+  # URI module doesn't support ws://
+  ws_url: "ws://127.0.0.1:5002/api/v0"
 
 import_config "#{config_env()}.exs"
