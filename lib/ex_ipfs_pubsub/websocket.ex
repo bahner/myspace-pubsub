9a2dc9dd26af6f537750328363e566abae555c53
Lars Bahner <lars.bahner@gmail.com>
2023-07-17 04:11:40 +0200

    Add Websocket struct

--- lib/ex_ipfs_pubsub/websocket.ex
+++ lib/ex_ipfs_pubsub/websocket.ex
@@ -5,7 +5,6 @@ defmodule ExIpfsPubsub.Websocket do
 
   require Logger
 
-
   @enforce_keys [:conn_pid, :stream_ref]
   defstruct conn_pid: nil, stream_ref: nil
 
@@ -13,15 +12,20 @@ defmodule ExIpfsPubsub.Websocket do
           conn_pid: pid(),
           stream_ref: reference()
         }
+  @doc """
+  Creates a new websocket connection to the PubSub daemon.
 
-  @spec new!(URI) :: t()
+  This is just a convenience function to create a new websocket connection
+  to make the Topic handler more readable.
+  """
+  @spec new!(%{:host => any, :path => any, :port => char, optional(any) => any}) :: t()
   def new!(url) when is_struct(url) do
-
     {:ok, conn_pid} = :gun.open(to_charlist(url.host), url.port)
     stream_ref = :gun.ws_upgrade(conn_pid, to_charlist(url.path), [])
 
     %__MODULE__{
       conn_pid: conn_pid,
-      stream_ref: stream_ref    }
+      stream_ref: stream_ref
+    }
   end
 end
